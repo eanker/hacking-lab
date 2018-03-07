2bdfceb93d2cb13b7aa82d8c16c07bd0f21bddf1
Eva Anker <E.S.Anker@student.tudelft.nl>
2018-03-07 10:09:49 +0100

    Changed install script and gitignore

--- installYate.sh
+++ installYate.sh
@@ -1,3 +1,5 @@
+echo "Start in git dir"
+
 # bladerf
 sudo add-apt-repository ppa:bladerf/bladerf -y
 sudo apt-get update
@@ -11,13 +13,14 @@ echo "# nuand bladeRF" | sudo tee -a /etc/udev/rules.d/90-yate.rules
 echo "ATTR{idVendor}==\"1d50\", ATTR{idProduct}==\"6066\", MODE=\"660\", GROUP=\"yate\"" | sudo tee -a /etc/udev/r$
 sudo udevadm control --reload-rules
 
-mkdir -p ~/software/null
-cd ~/software/null
-svn checkout http://yate.null.ro/svn/yate/trunk yate
-svn checkout http://voip.null.ro/svn/yatebts/trunk yatebts
+#Not needed as you have cloned this :P
+#mkdir -p ~/software/null
+#cd ~/software/null
+#svn checkout http://yate.null.ro/svn/yate/trunk yate
+#svn checkout http://voip.null.ro/svn/yatebts/trunk yatebts
 
 # yate
-cd ~/software/null/yate
+cd yate
 ./autogen.sh
 ./configure --prefix=/usr/local
 make
@@ -25,7 +28,7 @@ sudo make install
 sudo ldconfig
 
 # yatebts
-cd ~/software/null/yatebts
+cd ../yatebts
 ./autogen.sh
 ./configure --prefix=/usr/local
 make
